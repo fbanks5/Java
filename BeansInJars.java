9613bc79b015a79aa12d273af653ce34bcd918ab
Frantz Banks III <frantz7101990@gmail.com>
2024-08-07 22:31:49 -0400

    java course project commits

---
+++ BeansInJars.java
@@ -0,0 +1,15 @@
+public class BeansInJars {
+   public static void main (String [] args) {
+      int numBeans;
+      int numJars;
+      int totalBeans;
+
+      numBeans = 500;
+      numJars = 3;
+   
+      System.out.print(numBeans + " beans in ");
+      System.out.print(numJars + " jars yields ");
+      totalBeans = numBeans * numJars; 
+      System.out.println(totalBeans + " total");
+   }
+}
